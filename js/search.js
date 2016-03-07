3e47d328a7d136d79ff1dbeff5bcb817f2a4aeed
mryodo <a.s.savostyanov@gmail.com>
2016-03-07 13:18:11 +0300

    Minor fixes

--- js/search.js
+++ js/search.js
@@ -1,4 +1,4 @@
-jQuery(function() {
+  jQuery(function() {
   // Initalize lunr with the fields it will be searching on. I've given title
   // a boost of 10 to indicate matches on this field are more important.
   window.idx = lunr(function () {
